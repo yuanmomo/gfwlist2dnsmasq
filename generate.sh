9c81c27635e59867be185bfa45aa12281407187a
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2021-02-06 16:18:59 +0800

    add firwall

--- generate.sh
+++ generate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-bash gfwlist2dnsmasq.sh -d 10.10.1.4 -p 53 -o gfwlist.conf -s ipset --extra-domain-file gfwlist2domain.txt
+bash gfwlist2dnsmasq.sh -d 10.10.1.4 -p 53 -o gfwlist.conf -s gfwlist --extra-domain-file gfwlist2domain.txt
 
 scp gfwlist.conf home:/etc/dnsmasq.d
 scp ip_ext.txt home:/root
 
 
-ssh home "source /etc/profile && service dnsmasq restart"
+#ssh home "source /etc/profile && service dnsmasq restart"
+ssh home "/etc/init.d/dnsmasq restart"
 
 
