9e09b833d37f6a896a890b3ef37aa745311d944e
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2021-02-09 10:41:00 +0800

    add exclue

--- generate.sh
+++ generate.sh
@@ -2,6 +2,11 @@
 
 bash gfwlist2dnsmasq.sh -d 10.10.1.4 -p 53 -o gfwlist.conf -s gfwlist --extra-domain-file gfwlist2domain.txt
 
+
+grep -IFvf exclude_domain_regex  gfwlist.conf > gfwlist.conf.2
+
+mv -fvi gfwlist.conf.2 gfwlist.conf
+
 scp gfwlist.conf home:/etc/dnsmasq.d
 scp ip_ext.txt home:/root
 
